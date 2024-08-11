9d55ddf463414e1772439af750e144183a35dc6f
Rafael Alex <168346724+Rafaelshelby19@users.noreply.github.com>
2024-08-11 19:05:57 -0300

    Add files via upload

---
+++ fruta.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+interface NodeResponse {
+  message: string;
+  data?: any; // Defina esta parte conforme o que o servidor realmente retorna
+}
+
+@Component({
+  selector: 'app-fruta',
+  standalone: true,
+  imports: [ReactiveFormsModule, HttpClientModule],
+  templateUrl: './fruta.component.html',
+  styleUrls: ['./fruta.component.css']
+})
+export class FrutaComponent implements OnInit {
+  nodeForm: FormGroup;
+  isLoading = false;
+
+  constructor(private fb: FormBuilder, private http: HttpClient) {
+    this.nodeForm = this.fb.group({
+      nodeData: ['', Validators.required]
+    });
+  }
+
+  ngOnInit(): void {}
+
+  onSubmit(): void {
+    if (this.nodeForm.valid) {
+      this.isLoading = true;
+      this.http.post<NodeResponse>('http://localhost:3000/api/nodes', this.nodeForm.value).subscribe({
+        next: (response) => {
+          console.log('Node data submitted', response);
+          this.isLoading = false;
+        },
+        error: (error) => {
+          console.error('Error submitting node data', error);
+          this.isLoading = false;
+        }
+      });
+    }
+  }
+}
+
